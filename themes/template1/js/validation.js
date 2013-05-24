eef160cbcc5044068cc1c4c4f22799432e26227e
Amit Arora <eraroraamit@gmail.com>
2013-05-24 10:53:38 +0530

    checking

--- themes/template1/js/validation.js
+++ themes/template1/js/validation.js
@@ -1,8 +1,8 @@
 	$(document).ready(function() {
 			$('#submit').live('click',function(){
 				var filter = /^[0-9]+$/;
-				var rangestart= parseInt(jQuery.trim($('#rangestart').val())); 
-				var rangeend = parseInt(jQuery.trim($('#rangeend').val()));
+				var rangestart= jQuery.trim($('#rangestart').val()); 
+				var rangeend = jQuery.trim($('#rangeend').val());
 				if(rangestart == '' || rangeend == '' ){
 					$('#msg').show().html("Please fill all the value").css('color','red');	
 					return false;
@@ -12,7 +12,7 @@
 						$('#msg').show().html("Numeric Value Please").css('color','red');	
 						return false;
 					}
-					if(rangeend < rangestart){
+					if(parseInt(rangeend) < parseInt(rangestart)){
 						$('#msg').show().html("Start Range is always lesser than End Range").css('color','red');	
 							return false;
 						
